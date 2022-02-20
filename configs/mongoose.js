3a58ae6292dd270d8f2acc315a960099a7fb801f
Abhishek Patel <ap8209655@gmail.com>
2022-02-20 21:53:09 +0530

    Added mongodb cluster

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/Worldbook_Database", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost/Worldbook_Database", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+mongoose.connect(
+  "mongodb+srv://worldbookabhi:Coaderabhi@cluster0.zwitd.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
